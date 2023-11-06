89b179c265b131f4b1cae1070230003341d4852f
Pouya <popibani1379@gmail.com>
2023-11-06 10:09:30 +0330

    some major changes.

--- laboratory/admin.py
+++ laboratory/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from laboratory.models import Laboratory
+from laboratory.models import Laboratory, Category
 
 
 @admin.register(Laboratory)
 class LaboratoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["title"]}
+
+
+admin.site.register(Category)
--- laboratory/models.py
+++ laboratory/models.py
@@ -6,8 +6,8 @@ from django.utils.html import format_html
 class Laboratory(models.Model):
     title = models.CharField(max_length=50, verbose_name="تیتر", help_text="ضروری")
     # parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, verbose_name="والد")
-    has_parent = models.BooleanField(default=False, verbose_name="والد دارد؟", help_text="ضروری")
-    has_child = models.BooleanField(default=False, verbose_name="فرزند دارد؟", help_text="ضروری")
+    # has_parent = models.BooleanField(default=False, verbose_name="والد دارد؟", help_text="ضروری")
+    # has_child = models.BooleanField(default=False, verbose_name="فرزند دارد؟", help_text="ضروری")
     record_preview = models.CharField(max_length=200, verbose_name="درباره سابقه", help_text="ضروری")
     image_1 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 1", help_text="ضروری")
     image_2 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 2", help_text="ضروری")
@@ -18,9 +18,10 @@ class Laboratory(models.Model):
     image_7 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 7", blank=True, null=True)
     image_8 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 8", blank=True, null=True)
     body = RichTextField(max_length=50000, verbose_name="بدنه", unique=True, help_text="ضروری")
-    location = models.CharField(max_length=100, verbose_name="لوکیشن", help_text="ضروری")
-    financial_value = models.CharField(max_length=50, verbose_name="ارزش مالی", help_text="ضروری")
-    date = models.CharField(max_length=100, verbose_name="تاریخ ساخت سابقه", help_text="ضروری")
+    categories = models.ManyToManyField("Category", related_name='weblogs', verbose_name="کتگوری ها", help_text="ضروری")
+    # location = models.CharField(max_length=100, verbose_name="لوکیشن", help_text="ضروری")
+    # financial_value = models.CharField(max_length=50, verbose_name="ارزش مالی", help_text="ضروری")
+    # date = models.CharField(max_length=100, verbose_name="تاریخ ساخت سابقه", help_text="ضروری")
     is_allowed = models.BooleanField(default=True, verbose_name="مجوز نشان داده شدن دارد؟")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="ایجاد شده در تاریخ")
     slug = models.SlugField(unique=True, allow_unicode=True, blank=True, null=True, verbose_name="اسلاگ (متن داخل URL)")
@@ -36,3 +37,15 @@ class Laboratory(models.Model):
         verbose_name = "آزمایشگاه"
         verbose_name_plural = "آزمایشگاه ها"
         ordering = ("-created_at",)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=100, verbose_name="کتگوری", help_text="ضروری")
+    is_allowed = models.BooleanField(default=True, verbose_name="مجوز نشان داده شدن دارد؟")
+
+    class Meta:
+        verbose_name = "کتگوری"
+        verbose_name_plural = "کتگوری ها"
+
+    def __str__(self):
+        return self.category

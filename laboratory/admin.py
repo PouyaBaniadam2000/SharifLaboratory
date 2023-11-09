e1cca421451c215c9138debaf57b1307d83134bf
Pouya <popibani1379@gmail.com>
2023-11-09 19:32:20 +0330

    50% Back-end of nested categories have been completed!

--- laboratory/admin.py
+++ laboratory/admin.py
@@ -4,7 +4,34 @@ from laboratory.models import Laboratory, Category
 
 @admin.register(Laboratory)
 class LaboratoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", "laboratory_preview", "display_image_1", "display_image_2", "display_image_3", "display_image_4",
+        "is_allowed", "created_at")
+    list_editable = ("is_allowed",)
     prepopulated_fields = {"slug": ["title"]}
 
+    def display_image_1(self, obj):
+        return obj.show_image_1()
 
-admin.site.register(Category)
+    display_image_1.short_description = 'تصویر آزمایشگاه 1'
+
+    def display_image_2(self, obj):
+        return obj.show_image_2()
+
+    display_image_2.short_description = 'تصویر آزمایشگاه 2'
+
+    def display_image_3(self, obj):
+        return obj.show_image_3()
+
+    display_image_3.short_description = 'تصویر آزمایشگاه 3'
+
+    def display_image_4(self, obj):
+        return obj.show_image_4()
+
+    display_image_4.short_description = 'تصویر آزمایشگاه 4'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    prepopulated_fields = {"slug": ["title"]}
--- laboratory/models.py
+++ laboratory/models.py
@@ -3,25 +3,30 @@ from ckeditor.fields import RichTextField
 from django.utils.html import format_html
 
 
+class Category(models.Model):
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE, related_name="subs")
+    title = models.CharField(max_length=100, verbose_name="تیتر", help_text="ضروری")
+    slug = models.SlugField(allow_unicode=True, max_length=150, verbose_name="اسلاگ (متن داخل URL)", help_text="ضروری")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+
+
 class Laboratory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="دسته بندی", help_text="ضروری")
     title = models.CharField(max_length=50, verbose_name="تیتر", help_text="ضروری")
-    # parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, verbose_name="والد")
-    # has_parent = models.BooleanField(default=False, verbose_name="والد دارد؟", help_text="ضروری")
-    # has_child = models.BooleanField(default=False, verbose_name="فرزند دارد؟", help_text="ضروری")
-    record_preview = models.CharField(max_length=200, verbose_name="درباره سابقه", help_text="ضروری")
+    laboratory_preview = models.CharField(max_length=200, verbose_name="درباره آزمایشگاه", help_text="ضروری")
     image_1 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 1", help_text="ضروری")
     image_2 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 2", help_text="ضروری")
     image_3 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 3", help_text="ضروری")
     image_4 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 4", help_text="ضروری")
     image_5 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 5", blank=True, null=True)
     image_6 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 6", blank=True, null=True)
-    image_7 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 7", blank=True, null=True)
-    image_8 = models.ImageField(upload_to="laboratory/laboratory/images", verbose_name="تصویر 8", blank=True, null=True)
     body = RichTextField(max_length=50000, verbose_name="بدنه", unique=True, help_text="ضروری")
-    categories = models.ManyToManyField("Category", related_name='weblogs', verbose_name="کتگوری ها", help_text="ضروری")
-    # location = models.CharField(max_length=100, verbose_name="لوکیشن", help_text="ضروری")
-    # financial_value = models.CharField(max_length=50, verbose_name="ارزش مالی", help_text="ضروری")
-    # date = models.CharField(max_length=100, verbose_name="تاریخ ساخت سابقه", help_text="ضروری")
     is_allowed = models.BooleanField(default=True, verbose_name="مجوز نشان داده شدن دارد؟")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="ایجاد شده در تاریخ")
     slug = models.SlugField(unique=True, allow_unicode=True, blank=True, null=True, verbose_name="اسلاگ (متن داخل URL)")
@@ -29,23 +34,23 @@ class Laboratory(models.Model):
     def __str__(self):
         return self.title
 
-    def show_image(self):
+    def show_image_1(self):
         return format_html(
             f'<a href="{self.image_1.url}"><img style="border-radius: 100%" src="{self.image_1.url}" alt="{self.image_1.name}" width="30px" height="30px"></a>')
+
+    def show_image_2(self):
+        return format_html(
+            f'<a href="{self.image_2.url}"><img style="border-radius: 100%" src="{self.image_2.url}" alt="{self.image_2.name}" width="30px" height="30px"></a>')
+
+    def show_image_3(self):
+        return format_html(
+            f'<a href="{self.image_3.url}"><img style="border-radius: 100%" src="{self.image_3.url}" alt="{self.image_3.name}" width="30px" height="30px"></a>')
+
+    def show_image_4(self):
+        return format_html(
+            f'<a href="{self.image_4.url}"><img style="border-radius: 100%" src="{self.image_4.url}" alt="{self.image_4.name}" width="30px" height="30px"></a>')
 
     class Meta:
         verbose_name = "آزمایشگاه"
         verbose_name_plural = "آزمایشگاه ها"
         ordering = ("-created_at",)
-
-
-class Category(models.Model):
-    category = models.CharField(max_length=100, verbose_name="کتگوری", help_text="ضروری")
-    is_allowed = models.BooleanField(default=True, verbose_name="مجوز نشان داده شدن دارد؟")
-
-    class Meta:
-        verbose_name = "کتگوری"
-        verbose_name_plural = "کتگوری ها"
-
-    def __str__(self):
-        return self.category

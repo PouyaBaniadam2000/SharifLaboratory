0eb216353e4aab20aadcbe32ddd5b2b70f4d9a7b
YasinSamooei <samooei.y03@gmail.com>
2023-11-11 13:17:40 +0330

    fix showing laboratories related to an category

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -6,6 +6,7 @@ app_name = "laboratory"
 urlpatterns = [
     path("list", views.LaboratoryListView.as_view(), name="all_laboratories"),
     re_path(r'(?P<slug>[-\w]+)', views.LaboratoryDetailView.as_view(), name="laboratory_detail"),
-    re_path(r'category/(?P<slug>[-\w]+)', views.laboratory_category_list_view,
-            name='category_laboratories'),
+    # re_path(r'category/(?P<slug>[-\w]+)', views.laboratory_category_list_view,
+    #         name='category_laboratories'),
+    path('<str:slug>', views.CategoryDetailView.as_view(), name='category_laboratories'),
 ]
--- laboratory/views.py
+++ laboratory/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView , View
 from laboratory.mixins import IsAllowedMixin
 from laboratory.models import Laboratory, Category
-
+from urllib.parse import unquote
+from django.core.paginator import Paginator
 
 class LaboratoryListView(ListView):
     model = Laboratory
@@ -34,15 +35,35 @@ class LaboratoryDetailView(IsAllowedMixin, DetailView):
         return context
 
 
-def laboratory_category_list_view(request, slug):
-    print(slug)
-    category = Category.objects.get(slug=slug)
-    print(category)
-    laboratories = Laboratory.objects.filter(category=category)
+# def laboratory_category_list_view(request, slug):
+#     print(slug)
+#     category = Category.objects.get(slug=slug)
+#     print(category)
+#     laboratories = Laboratory.objects.filter(category=category)
+#
+#     context = {
+#         'category': category,
+#         'laboratories': laboratories,
+#     }
+#
+#     return render(request, 'laboratory/laboratory_category_list.html', context)
 
-    context = {
-        'category': category,
-        'laboratories': laboratories,
-    }
 
-    return render(request, 'laboratory/laboratory_category_list.html', context)
+class CategoryDetailView(View):
+    """
+    View for returning laboratory
+    of the selected category
+    """
+
+    def get(self, request, slug):
+        slug = unquote(slug)
+        category = get_object_or_404(Category, slug=slug)
+        laboratory = Laboratory.objects.filter(category__title=category)
+
+        # pagination
+        page_number = request.GET.get('page')
+        paginator = Paginator(laboratory, 15)
+        objects_list = paginator.get_page(page_number)
+
+        context = {"laboratories": objects_list, "category": category}
+        return render(request, 'laboratory/laboratory_category_list.html', context)
